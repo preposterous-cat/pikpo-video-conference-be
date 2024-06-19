ba9078860d35115d5e544dfca5fea249c73f3b53
Arizli Romadhon <arielromadhon7@gmail.com>
2024-06-19 21:13:27 +0700

    adding cors

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ import participantRoute from "./routes/participantRoute.js";
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(
+  cors({
+    origin: "*", // Allow all origins - adjust as needed
+  })
+);
+
 app.use("/api/token", tokenRoutes);
 app.use("/api/participant", participantRoute);
 
