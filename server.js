d37bc3c36315634ba9fcf9dbdb864eeec9b26e04
Arizli Romadhon <arielromadhon7@gmail.com>
2024-06-20 10:34:03 +0700

    delete cors

--- server.js
+++ server.js
@@ -6,12 +6,6 @@ import participantRoute from "./routes/participantRoute.js";
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(
-  cors({
-    origin: "*", // Allow all origins - adjust as needed
-  })
-);
-
 app.use("/api/token", tokenRoutes);
 app.use("/api/participant", participantRoute);
 
